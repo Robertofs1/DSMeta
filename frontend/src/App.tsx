ad0b8fc45058b584c33afcd227119ab6393cc7fb
Robertofs1 <frisancoroberto@gmail.com>
2022-07-12 22:57:29 -0300

    First Component created

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import NotificationBtn from "./components/notificationBtn"
+
+function App() {
+  return (
+    <>
+    <h1>Hallo</h1>
+    <NotificationBtn />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/notificationBtn/index.tsx
@@ -0,0 +1,12 @@
+import icon from '../../assets/image/notifyicon.svg'
+import './Styles.css'
+function NotificationBtn() {
+    return (
+        <div className="dsmeta-red-btn">
+        <img src={icon} alt="Notificar" />
+      </div>
+    )
+  }
+  
+  export default NotificationBtn
+  
